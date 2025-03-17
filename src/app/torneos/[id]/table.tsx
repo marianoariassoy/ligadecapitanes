c250389c9cd9c3d677b4aa9dd94f5355c5ec02ce
marianoariassoy <hola@marianoarias.soy>
2025-03-17 11:24:16 -0300

    first commit

--- src/app/torneos/[id]/table.tsx
+++ src/app/torneos/[id]/table.tsx
@@ -86,21 +86,21 @@ const Tabla = async ({ group, type }: { group: Group; type: number }) => {
                             <Bull />
                           </span>
                         ) : (
-                          <span className="text-red-400">
+                          <span className="text-white/40">
                             <Bull />
                           </span>
                         )}
                       </span>
                     ))}
-                    {item.series.length === 0 ? (
-                      <span className="text-white/30 flex gap-x-2">
-                        <Bull />
-                        <Bull />
-                        <Bull />
-                        <Bull />
-                        <Bull />
-                      </span>
-                    ) : null}
+                    {Array(5 - item.series.length)
+                      .fill(0)
+                      .map((_, index) => (
+                        <span key={index}>
+                          <span className="text-white/10">
+                            <Bull />
+                          </span>
+                        </span>
+                      ))}
                   </div>
                 </td>
               </tr>
