e90eb182102aecfb55d0e61f8ce35d79711f8c65
marianoariassoy <hola@marianoarias.soy>
2026-08-10 22:29:09 -0300

    first commit

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,13 +1,13 @@
 export const categories = [
   {
     name: "Caballeros Intermedia +35",
-    url: "/torneos/5",
+    url: "/torneos/7",
     slug: "intermedia",
     id: 13,
   },
   {
     name: "Caballeros Segunda +35",
-    url: "/torneos/6",
+    url: "/torneos/8",
     slug: "segunda",
     id: 12,
   },
